9d0249b4445c336f33e1e5e2efbf4d94742a008f
Daksh Kitukale <97655058+K-Daksh@users.noreply.github.com>
2024-11-23 20:06:07 +0530

    Update compile.js

--- compile.js
+++ compile.js
@@ -4,11 +4,10 @@ const options = {
     method: 'POST',
     url: 'https://code-compiler10.p.rapidapi.com/',
     headers: {
-        'content-type': 'application/json',
-        'x-compile': 'rapidapi',
-        'Content-Type': 'application/json',
-        'X-RapidAPI-Key': '79bf21192fmsh570c3bfdfed20aep16e351jsnc0fa0eb78944',
-        'X-RapidAPI-Host': 'code-compiler10.p.rapidapi.com'
+        "x-rapidapi-key": "79bf21192fmsh570c3bfdfed20aep16e351jsnc0fa0eb78944",
+	"x-rapidapi-host": "code-compiler10.p.rapidapi.com",
+	"Content-Type": "application/json",
+	"x-compile": "rapidapi"
     },
     body: {
         langEnum: [
